70cbe3e25e76b21ee2e1e95bba4cffd71b6a5859
bradhilton <brad.hilton.nw@gmail.com>
2016-04-01 09:28:08 -0600

    Initial Commit

---
+++ OrderedDictionary/OrderedDictionary.h
@@ -0,0 +1,19 @@
+//
+//  OrderedDictionary.h
+//  OrderedDictionary
+//
+//  Created by Bradley Hilton on 4/1/16.
+//  Copyright © 2016 Brad Hilton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OrderedDictionary.
+FOUNDATION_EXPORT double OrderedDictionaryVersionNumber;
+
+//! Project version string for OrderedDictionary.
+FOUNDATION_EXPORT const unsigned char OrderedDictionaryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OrderedDictionary/PublicHeader.h>
+
+
